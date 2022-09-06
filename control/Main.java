b342f57a60173a2aa6cc86423d0e8fe55a2dfeb4
Diego Arango <crash.diego91@gmail.com>
2022-09-06 17:21:55 -0500

    Arreglos, ciclos e instrucciones de control

--- control/Main.java
+++ control/Main.java
@@ -1,6 +1,7 @@
 package control;
 
 import model.Cliente;
+import model.Empleado;
 import model.Empresa;
 
 import java.util.ArrayList;
@@ -15,7 +16,31 @@ public class Main {
     public static void main(String[] args) {
         Empresa carvajal = new Empresa("Carvajal");
         Cliente cliente1 = new Cliente("Clientesito", 22, "3111112233");
+
+        boolean[] estado = new boolean[5];
+        char[] sexo = new char[2];
+        String[] nombre = new String[2];
+        Cliente[] clientes = new Cliente[10];
+
+        int[] edad = {45, 23, 11, 9};  //Array de 4 elementos
+
+        System.out.println(edad[1]); // Imprime 23
+        System.out.println(edad.length); // Imprime 4
+
+
+        Empleado empleado1 = new Empleado(
+                "Pepito Perez",
+                31,
+                5000);
+        empleado1.deporteQuePractica("Tenis");
+
         boolean agregado = agregarCliente(cliente1, carvajal);
+
+        contarNumero(6);
+        contarNumeroBreak(6);
+
+        convertCadenaANumero("34");
+        convertCadenaANumero("Abc");
     }
 
 
@@ -79,6 +104,62 @@ public class Main {
     public static void imprimirLista(String[] listaNombres){
         for (String nombre : listaNombres) {
             System.out.println(nombre);
+        }
+    }
+
+    public static void contarNumeroBreak(int limite){
+        for(int i = 0; i <= limite; i++){
+            if (i == 5){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+
+    public static void contarNumeroContinue(int limite){
+        for(int i = 0; i <= limite; i++){
+            if (i == 5){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+
+    public static int contarNumero(int limite){
+        int cont = 0;
+        while(true) {
+            if(cont == limite) {
+                return cont;
+            }
+            cont++;
+        }
+    }
+
+    public static String esPar(int numero){
+        if(numero % 2 == 0){
+            return "Es par";
+        }
+        // Bloque de codigo que se ejecuta si
+        // no se cumple la condicion especificada
+        // en el bloque anterior
+        return "No es par";
+    }
+
+    public static void convertCadenaANumero(String cadena){
+        try {
+            int numero = Integer.parseInt(cadena);
+            System.out.println("La cadena si es un numero");
+        } catch (NumberFormatException  e) {
+            System.out.println("Excepcion capturada, " +
+                    "la cadena no es un numero: "+ cadena);
+        }
+    }
+
+    public void cadenaVaciaONula(String cadena) throws Exception {
+        if(cadena.length() == 0) {
+            throw new Exception("Esta cadena es vacia");
+        } else {
+            System.out.println("No es una cadena vacia");
         }
     }
 }
